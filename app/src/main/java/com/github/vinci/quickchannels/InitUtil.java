e2812775b7202336459b9a02e7b68339f1a4fd47
hzhyq <hzhyq@tairanchina.com>
2017-04-17 18:25:44 +0800

    get channel with InitUtil

---
+++ app/src/main/java/com/github/vinci/quickchannels/InitUtil.java
@@ -0,0 +1,66 @@
+package com.github.vinci.quickchannels;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+/**
+ * Created by hzhyq on 2017/3/28 0028.
+ */
+
+public class InitUtil {
+    public static String getChannel(Context context) {
+        ApplicationInfo appinfo = context.getApplicationInfo();
+        String sourceDir = appinfo.sourceDir;
+        String ret = "";
+        ZipFile zipfile = null;
+        try {
+            zipfile = new ZipFile(sourceDir);
+            Enumeration<?> entries = zipfile.entries();
+            while (entries.hasMoreElements()) {
+                ZipEntry entry = ((ZipEntry) entries.nextElement());
+                String entryName = entry.getName();
+                if (entryName.startsWith("META-INF/pwchannel")) {
+                    ret = entryName;
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (zipfile != null) {
+                try {
+                    zipfile.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        String[] split = ret.split("-");
+        if (split != null && split.length == 3) {
+            String channel = split[2];
+            return channel;
+
+        } else {
+            return "";
+        }
+    }
+
+    public static String getPackageVersion(Context context) {
+        PackageManager manager = context.getPackageManager();
+        try {
+            PackageInfo info = manager.getPackageInfo(context.getPackageName(), 0);
+            return info.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return "version_unknown";
+    }
+}
